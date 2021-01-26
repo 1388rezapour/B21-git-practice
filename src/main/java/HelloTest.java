c52da6735affa4b0e56f1313e3b86daa76dc875e
1388rezapour <haniehrezapour977@gmail.com>
2021-01-26 12:23:45 -0800

    master branch changed

--- src/main/java/HelloTest.java
+++ src/main/java/HelloTest.java
@@ -1,7 +1,7 @@
 public class HelloTest {
 
     public static void main(String[] args) {
-        System.out.println("Hello world, 21");
+        System.out.println("Hello world, 21 good");
         System.out.println("B21 future SDET");
         // watching more movie
 
