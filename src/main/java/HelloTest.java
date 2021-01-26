4987845aeafb6e1ddbbd427695f197ddadd3a618
1388rezapour <haniehrezapour977@gmail.com>
2021-01-26 12:24:56 -0800

    action

--- src/main/java/HelloTest.java
+++ src/main/java/HelloTest.java
@@ -3,6 +3,7 @@ public class HelloTest {
     public static void main(String[] args) {
         System.out.println("Hello world, 21 good");
         System.out.println("B21 future SDET");
+        System.out.println("more action");
         // watching more movie
 
 
